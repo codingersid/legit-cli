a63b8e0e544d8d97d98c4f54b394e23d5617df04
frandika06 <frandika.septa@gmail.com>
2024-05-01 00:52:35 +0700

    hello

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
+import "fmt"
 
-	"github.com/codingersid/legit-cli/cmd"
-)
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: filecreator [filename] [content]")
-		os.Exit(1)
-	}
-
-	filename := os.Args[1]
-	content := os.Args[2]
-
-	err := cmd.CreateFile(filename, content)
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("File", filename, "created successfully!")
+func Hello() {
+	fmt.Println("Hello from example package!")
 }
